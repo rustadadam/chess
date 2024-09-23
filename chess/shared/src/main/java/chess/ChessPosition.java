9517135750e5b572a98b333a2cfbe97d306b49f9
Adam Rustad <149918639+rustadadam@users.noreply.github.com>
2024-09-23 14:05:46 -0600

    Chess Files I created after the test

---
+++ chess/shared/src/main/java/chess/ChessPosition.java
@@ -0,0 +1,56 @@
+package chess;
+
+/**
+ * Represents a single square position on a chess board
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPosition {
+
+    private int row;
+    private int col;
+
+    public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    /**
+     * @return which row this position is in
+     * 1 codes for the bottom row
+     */
+    public int getRow() {
+        return row;
+        //throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * @return which column this position is in
+     * 1 codes for the left row
+     */
+    public int getColumn() {
+        return col;
+        //throw new RuntimeException("Not implemented");
+    }
+
+
+    //Overide
+    @Override
+    public String toString() {
+        return row + "," + col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode(){
+        return 31 * row + col;
+    }
+}
