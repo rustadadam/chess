d33dd67584025ff5eed1467d7d37bbbbbbe79322
rustadadam <rustadadam@gmail.com>
2024-10-09 16:49:03 -0600

    Getting started code?

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,7 +2,8 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        var server = new Server();
+        var port = server.run(8080);
+        System.out.println("♕ 240 Chess Server started on: " + port);
     }
 }
