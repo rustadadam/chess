d3895d5a461745a280d82728a546cf9f4b16c06f
rustadadam <rustadadam@gmail.com>
2024-10-09 14:31:43 -0600

    Added Pet files, and cleaned repository

---
+++ server/src/main/java/Server.java
@@ -0,0 +1,18 @@
+public class Server {
+    public static void main(String[] args) {
+        var s = new Server();
+        var port = s.run(0);
+        System.out.println("Running on port: " + port);
+    }
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        Spark.get("/hello", (req, res) -> "hello");
+
+        Spark.init();
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+}
