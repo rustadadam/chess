dadb0e9b61c81fa1e4308510e6dd54f79498e79f
rustadadam <rustadadam@gmail.com>
2024-10-21 11:55:25 -0600

    Elimated some if statements.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -161,13 +161,11 @@ public class ChessPiece {
                     if (availableMoves.size() < 3) {
                         availableMoves.add(move);
                     }
-                    if (myPosition.getRow() == 7) {
-                        newPosition = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn());
-                        if (board.getPiece(newPosition) == null) {
-                            move = new ChessMove(myPosition, newPosition, null);
-                            availableMoves.add(move);
-                            checkPromotion(board, myPosition, newPosition);
-                        }
+                    newPosition = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn());
+                    if (myPosition.getRow() == 7 && board.getPiece(newPosition) == null) {
+                        move = new ChessMove(myPosition, newPosition, null);
+                        availableMoves.add(move);
+                        checkPromotion(board, myPosition, newPosition);
                     }
                 }
                 newPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1);
@@ -207,22 +205,18 @@ public class ChessPiece {
                     }
                 }
                 newPosition = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1);
-                if (board.getPiece(newPosition) != null) {
-                    if (board.getPiece(newPosition).getTeamColor() != color) {
-                        ChessMove move = new ChessMove(myPosition, newPosition, null);
-                        checkPromotion(board, myPosition, newPosition);
-                        if (availableMoves.size() < 3) {
-                            availableMoves.add(move);
-                        }
+                if (board.getPiece(newPosition) != null && board.getPiece(newPosition).getTeamColor() != color) {
+                    ChessMove move = new ChessMove(myPosition, newPosition, null);
+                    checkPromotion(board, myPosition, newPosition);
+                    if (availableMoves.size() < 3) {
+                        availableMoves.add(move);
                     }
                 }
                 newPosition = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1);
-                if (board.getPiece(newPosition) != null) {
-                    if (board.getPiece(newPosition).getTeamColor() != color) {
-                        ChessMove move = new ChessMove(myPosition, newPosition, null);
-                        availableMoves.add(move);
-                        checkPromotion(board, myPosition, newPosition);
-                    }
+                if (board.getPiece(newPosition).getTeamColor() != color && board.getPiece(newPosition) != null) {
+                    ChessMove move = new ChessMove(myPosition, newPosition, null);
+                    availableMoves.add(move);
+                    checkPromotion(board, myPosition, newPosition);
                 }
             }
         }
