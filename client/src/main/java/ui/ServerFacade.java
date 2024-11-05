683d5e6172c96d04e51a1cf5d3e8bab698ed08d8
rustadadam <rustadadam@gmail.com>
2024-11-04 20:17:49 -0700

    Server Facade Updates

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,98 @@
+package ui;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+    public void clearDataBase() throws Exception {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public void deletePet(int id) throws Exception {
+        var path = String.format("/pet/%s", id);
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public void deleteAllPets() throws Exception {
+        var path = "/pet";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public Pet[] listPets() throws Exception {
+        var path = "/pet";
+        record listPetResponse(Pet[] pet) {
+        }
+        var response = this.makeRequest("GET", path, null, listPetResponse.class);
+        return response.pet();
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, Exception {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new Exception("failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
+
+}
