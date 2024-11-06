049c39e183690e96066c7a4efc00d1d149208486
rustadadam <rustadadam@gmail.com>
2024-11-05 17:36:55 -0700

    Implemented First Server Facade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,21 @@
 package client;
 
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static String serverUrl = "http://localhost:8080";
+    private static ServerFacade serverFacade = new ServerFacade(serverUrl);
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -23,6 +27,21 @@ public class ServerFacadeTests {
 
     @Test
     public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    //Tests to ensure the server facade works
+    public void serverFacadeClear() throws Exception {
+        serverFacade.clearDataBase();
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    //Tests to ensure the server facade works
+    public void registerSuccess() throws Exception {
+        UserData userData = new UserData("Adam", "Austria", "Email");
+        serverFacade.register(userData);
         Assertions.assertTrue(true);
     }
 
