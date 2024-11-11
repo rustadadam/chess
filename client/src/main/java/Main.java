18e51cbe071244a9b0a0a7226966f7627fff4608
rustadadam <rustadadam@gmail.com>
2024-11-11 14:03:12 -0700

    Update Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,12 +3,15 @@ import ui.Repl;
 
 import static ui.EscapeSequences.*;
 
+//Note -> Work on storing auths and then do the http add request property
+
+
 public class Main {
     public static void main(String[] args) {
 
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println(RESET_TEXT_COLOR + "♕ 240 Chess Client: " + piece);
-        
+
         Repl repl = new Repl("http://localhost:8080");
         repl.run();
     }
