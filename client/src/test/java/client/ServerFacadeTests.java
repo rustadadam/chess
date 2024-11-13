e640fa5a5883fee5cb827799cc6d117eb1c92bb2
rustadadam <rustadadam@gmail.com>
2024-11-12 20:11:58 -0700

    Added more tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.*;
 import server.Server;
 import service.GameService;
 import ui.LoginClient;
+import ui.PreLoginClient;
 import ui.ServerFacade;
+import ui.State;
 
 import java.util.Collection;
 
@@ -61,7 +63,7 @@ public class ServerFacadeTests {
         AuthData auth = serverFacade.register(userData);
 
         try {
-            AuthData auth2 = serverFacade.register(userData);
+            serverFacade.register(userData);
             Assertions.fail();
         } catch (Exception e) {
             Assertions.assertNotNull(auth);
@@ -123,6 +125,158 @@ public class ServerFacadeTests {
         LoginClient client = new LoginClient(serverUrl, authData.authToken());
         try {
             client.create();
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Success to observe game")
+    public void successObserveGamesTest() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        UserData userData = new UserData("slypuppy", "Mongi", "Email");
+        AuthData authData = serverFacade.register(userData);
+
+        LoginClient client = new LoginClient(serverUrl, authData.authToken());
+        client.create("new");
+        String str = client.observe("1");
+        if (str.contains("Error")) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    @DisplayName("Failure to observe game")
+    public void failObserveGamesTest() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        UserData userData = new UserData("slypuppy", "Mongi", "Email");
+        AuthData authData = serverFacade.register(userData);
+
+        LoginClient client = new LoginClient(serverUrl, authData.authToken());
+
+        try {
+            client.observe("1");
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Success to give help")
+    public void successHelpGamesTest() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        UserData userData = new UserData("Man", "Mongi", "Email");
+        AuthData authData = serverFacade.register(userData);
+
+        LoginClient client = new LoginClient(serverUrl, authData.authToken());
+        String str = client.help();
+        Assertions.assertNotNull(str);
+    }
+
+    @Test
+    @DisplayName("Success to change clients")
+    public void changeClientsTestSuccess() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        UserData userData = new UserData("slypuppy", "Mongi", "Email");
+        serverFacade.register(userData);
+
+        PreLoginClient client = new PreLoginClient(serverUrl);
+        client.login("slypuppy", "Mongi");
+
+        Assertions.assertEquals(client.getState(), State.SIGNEDIN);
+    }
+
+    @Test
+    @DisplayName("Failure to change clients")
+    public void changeClientsTestFailure() throws Exception {
+        serverFacade.clearDataBase();
+
+        PreLoginClient client = new PreLoginClient(serverUrl);
+        try {
+            client.login("slypuppy", "Dave");
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertNotNull(e);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Register clients")
+    public void registerClientsTestSuccess() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        PreLoginClient client = new PreLoginClient(serverUrl);
+        client.register("slypuppy", "Dave", "email");
+        client.login("slypuppy", "Dave");
+        Assertions.assertEquals(client.getState(), State.SIGNEDIN);
+    }
+
+    @Test
+    @DisplayName("Fail to Register clients")
+    public void registerClientsTestFail() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        PreLoginClient client = new PreLoginClient(serverUrl);
+
+        try {
+            client.register("myDawg", "Dave");
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertNotNull(e);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Overall Client Tests")
+    public void overallClientsTestSucceed() throws Exception {
+        serverFacade.clearDataBase();
+
+        //Register user
+        PreLoginClient client = new PreLoginClient(serverUrl);
+        //Register user
+        UserData userData = new UserData("Man", "Mongi", "Email");
+        AuthData authData = serverFacade.register(userData);
+
+        LoginClient loginClient = new LoginClient(serverUrl, authData.authToken());
+
+        client.register("Adam", "hi", "me");
+        Assertions.assertEquals(client.getState(), State.SIGNEDIN);
+
+        loginClient.create("NewGame");
+        loginClient.join("1", "WHITE");
+        Assertions.assertEquals(loginClient.getState(), State.INGAME);
+    }
+
+    @Test
+    @DisplayName("Overall Client Tests Fail")
+    public void overallClientsTestFail() throws Exception {
+        serverFacade.clearDataBase();
+
+        UserData userData = new UserData("Man", "Mongi", "Email");
+        AuthData authData = serverFacade.register(userData);
+
+        LoginClient loginClient = new LoginClient(serverUrl, authData.authToken());
+
+        loginClient.create("NewGame2");
+        loginClient.join("1", "WHITE");
+        Assertions.assertEquals(loginClient.getState(), State.INGAME);
+        
+        try {
+            loginClient.join("2", "WHITE");
             Assertions.fail();
         } catch (Exception e) {
             Assertions.assertNotNull(e);
