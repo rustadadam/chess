79b184c9b10d85b708289dcbec2287a50bc3e696
rustadadam <rustadadam@gmail.com>
2024-11-19 11:22:21 -0700

    Add connection function

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -5,10 +5,12 @@ import exception.ResponseException;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Timer;
 
 // This file just sends messages back and forth. It only needs to update the game in database
@@ -16,20 +18,31 @@ import java.util.Timer;
 @WebSocket
 public class WebSocketHandler {
 
-    private final ConnectionManager connections = new ConnectionManager();
+    private final HashMap<Integer, ConnectionManager> connections = new HashMap<>();
+    //private final ConnectionManager connections = new ConnectionManager();
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
         UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
         switch (action.getCommandType()) {
             case CONNECT -> connect(action.getGameID(), action.getAuthToken());
-            case MAKE_MOVE -> enter(action.visitorName(), session);
+            case MAKE_MOVE -> makeMove(message, session);
             case LEAVE -> exit(action.visitorName());
             case RESIGN -> resign(action.resign());
         }
     }
 
-    private void enter(String visitorName, Session session) throws IOException {
+    public void addConnection(Integer gameID) {
+        if (!connections.containsKey(gameID)) {
+            connections.put(gameID, new ConnectionManager());
+        }
+    }
+
+    private void makeMove(String message, Session session) throws IOException {
+        //Hydrate class
+        MakeMoveCommand action = new Gson().fromJson(message, MakeMoveCommand.class);
+
+
         connections.add(visitorName, session);
         var message = String.format("%s is in the shop", visitorName);
         var notification = new ServerMessage(UserGameCommand.CommandType.LEAVE, message);
